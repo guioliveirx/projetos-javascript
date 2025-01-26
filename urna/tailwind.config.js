c40f008a23adf49d70f386bf7cc64f0215f565a9
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-26 08:43:12 -0300

    feat: create keyframe pisca

--- urna/tailwind.config.js
+++ urna/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pisca: {
+          '0%': {
+            opacity: '0.1'
+          },
+          '50%': {
+            opacity: '1'
+          },
+          '100%': {
+            opacity: '0.2'
+          }
+        }
+      },
+      animation: {
+        pisca: "pisca 1s ease infinite"
+      },
+    },
   },
   plugins: [],
 }
