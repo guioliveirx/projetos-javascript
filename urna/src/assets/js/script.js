335d877bbc66cbd834fd5e9165a3f62f4eb9f99e
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-26 19:50:20 -0300

    feat: define all dom manipulate elements

---
+++ urna/src/assets/js/script.js
@@ -0,0 +1,10 @@
+let votoPara = document.querySelector("#votoPara span");
+let cargo = document.querySelector("#cargo");
+let descricao = document.querySelector("#descricao")
+let aviso = document.querySelector("#aviso");
+let candidatos = document.querySelector("#candidatos");
+let numeros = document.querySelector("#numeros");
+
+
+
+console.log(numerosTeclado);
