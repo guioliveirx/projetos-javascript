4d6739fcbaea5719a784c37943b6f4c5437eefda
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-31 12:51:44 -0300

    feat: create function comecar

--- urna/src/assets/js/script.js
+++ urna/src/assets/js/script.js
@@ -5,6 +5,24 @@ let aviso = document.querySelector("#aviso");
 let candidatos = document.querySelector("#candidatos");
 let numeros = document.querySelector("#numeros");
 
+let etapaAtual = 0;
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numerosHtml = '';
+    
+        for(let i = 0; i<etapa.numeros ; i++){
+            numerosHtml += '<div class="numero inline-block h-14 w-12 border border-black text-center leading-[48px] text-2xl"></div>';
+        }
+
+        votoPara.style.display = 'none';
+        cargo.innerHTML = etapa.titulo;
+        descricao.innerHTML = '';
+        aviso.style.display = 'none';
+        candidatos.innerHTML = '';
+        numeros.innerHTML = numerosHtml;
+}
+
 function clicou(number){
     alert("Clicou em: " + number);
 }
@@ -21,3 +39,4 @@ function confirmar() {
     alert("Voto CONFIRMADO!");
 }
 
+comecarEtapa();
