2c93f994c134c7264583f7145e2661da90f505c2
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-04 10:36:40 -0300

    feat: add animated number input and update interface on completion

--- urna/src/assets/js/script.js
+++ urna/src/assets/js/script.js
@@ -6,13 +6,18 @@ let candidatos = document.querySelector("#candidatos");
 let numeros = document.querySelector("#numeros");
 
 let etapaAtual = 0;
+let numero = '';
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
     let numerosHtml = '';
     
         for(let i = 0; i<etapa.numeros ; i++){
-            numerosHtml += '<div class="numero inline-block h-14 w-12 border border-black text-center leading-[48px] text-2xl"></div>';
+            if(i === 0) {
+                numerosHtml += '<div class="numero animate-pisca inline-block h-14 w-12 border border-black text-center leading-[48px] text-2xl"></div>';
+            }else {
+                numerosHtml += '<div class="numero inline-block h-14 w-12 border border-black text-center leading-[48px] text-2xl"></div>';
+            }
         }
 
         votoPara.style.display = 'none';
@@ -23,8 +28,23 @@ function comecarEtapa() {
         numeros.innerHTML = numerosHtml;
 }
 
+function atualizaInterface() {
+    alert("Finalizou a digitação do voto!");
+}
+
 function clicou(number){
-    alert("Clicou em: " + number);
+    let elementNumber = document.querySelector(".numero.animate-pisca");
+    if(elementNumber !== null){
+        elementNumber.innerHTML = number;
+        numero = `${numero}${number}`;
+
+        elementNumber.classList.remove("animate-pisca");
+        if(elementNumber.nextElementSibling !== null) {
+            elementNumber.nextElementSibling.classList.add("animate-pisca");
+        } else {
+            atualizaInterface();
+        }
+    }
 }
 
 function branco() {
