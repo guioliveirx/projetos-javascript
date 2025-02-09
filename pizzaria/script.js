cda682fe9a142c7c9ac7d074c04627eaddd6fe6f
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-09 14:39:40 -0300

    feat: create action to close modal

--- pizzaria/script.js
+++ pizzaria/script.js
@@ -1,3 +1,6 @@
+// Variável para armazenar a quantidade de pizzas de cada modal
+let quantidadePizzasModal;
+
 // Função para extrair o elemento do DOM de forma simplificada;
 const selector = (element) => document.querySelector(element);
 const selectorAll = (element) => document.querySelectorAll(element);
@@ -9,7 +12,11 @@ let pizzaModal = selector(".pizzaWindowArea");
 pizzaJson.map( ( {id, name, img, price, sizes, description}, index) => {
     // .cloneNode, clone a estrutura de um documento e todos os descendentes dele.
     let pizzaItem = selector(".models .pizza-item").cloneNode(true);
-    
+
+    // Adiciona um atributo de informação na div
+    pizzaItem.setAttribute("data-key", index);
+
+    // Informações dos cards das pizzas
     pizzaItem.querySelector(".pizza-item--img img").src = img;
     pizzaItem.querySelector(".pizza-item-price").innerHTML = `R$ ${price.toFixed(2)}`;
     pizzaItem.querySelector(".pizza-item-name").innerHTML = name;
@@ -18,7 +25,26 @@ pizzaJson.map( ( {id, name, img, price, sizes, description}, index) => {
     // Evento de click para o modal aparecer quando clicar na pizza
     pizzaItem.querySelector("a").addEventListener('click', (event) => {
         event.preventDefault();
+        
+        quantidadePizzasModal = 1;
+        let key = event.target.closest(".pizza-item").getAttribute("data-key");
+        let pizza = pizzaJson[key];
 
+        // Informações do modal das pizzas
+        selector(".pizzaBig img").src = pizza.img;
+        selector(".pizzaInfo h1").innerHTML = pizza.name;
+        selector(".pizzaInfo--desc").innerHTML = pizza.description;
+        selector(".pizzaInfo--actualPrice").innerHTML = `R$ ${pizza.price.toFixed(2)}`
+        selector(".pizzaInfo--size.selected").classList.remove("selected");
+        selectorAll(".pizzaInfo--size span").forEach( (size, index) => {
+            if( index === 2 ) {
+                size.closest(".pizzaInfo--size").classList.add("selected");
+            }
+            size.innerHTML = sizes[index];
+        });
+        selector(".pizzaInfo--qt").innerHTML = quantidadePizzasModal;
+
+        // Animação de fade-in para a exibição do modal
         pizzaModal.style.display = "flex";
         pizzaModal.style.opacity = 0;
         setTimeout( () => {
@@ -30,3 +56,11 @@ pizzaJson.map( ( {id, name, img, price, sizes, description}, index) => {
     // Adiciona o conteúdo das pizzas no "pizzaHtml"
     pizzaHtml.append( pizzaItem );
 });
+
+// Eventos MODAL
+function closeModal() {
+    pizzaModal.style.opacity = 0;
+    setTimeout( () => {
+        pizzaModal.style.display = "none";
+    }, 500);
+}
